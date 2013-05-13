afffacdb4c7f03d885adf091cb83b3c0ae51dce1
Aaron VonderHaar <gruen0aermel@gmail.com>
2013-05-12 21:06:20 -0700

    Move Rect from uilayer-core.

--- src/test/java/net/avh4/math/geometry/features/Agent.java
+++
@@ -1,4 +0,0 @@
-package net.avh4.math.geometry.features;
-
-public class Agent {
-}
--- src/test/java/net/avh4/math/geometry/features/FeaturesTest.java
+++
@@ -1,8 +0,0 @@
-package net.avh4.math.geometry.features;
-
-import cucumber.api.junit.Cucumber;
-import org.junit.runner.RunWith;
-
-@RunWith(Cucumber.class)
-public class FeaturesTest {
-}
